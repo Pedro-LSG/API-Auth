6c94edcae4345a416ae6d3b063daf3a906731a47
unknown <pedroluis20202@gmail.com>
2024-04-23 19:56:30 -0300

    [FEAT] Implementing own authorization settings

---
+++ auth/src/main/java/com/pedrogomes/auth/infra/security/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.pedrogomes.auth.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.pedrogomes.auth.domain.models.UserRole;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> 
+                auth.requestMatchers(HttpMethod.POST, "/product").hasRole(UserRole.ADMIN.getRole())
+                    .requestMatchers(HttpMethod.GET, "/product/{productName}").hasRole(UserRole.USER.getRole())
+                    .anyRequest().permitAll()
+            )
+            .build();
+    }
+}
